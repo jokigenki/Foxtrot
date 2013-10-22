9ed006ae56832dd62cb60f4d4ee120fff0277318
Tim Frost <tim@timf.me>
2013-10-22 15:02:40 +0100

    updated iOS icon. Updated Outside BG

---
+++ behavior-hx/Design_74_74_WalkingPM.hx
@@ -0,0 +1,314 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.events.Event;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import com.eclecticdesignstudio.motion.Actuate;
+import com.eclecticdesignstudio.motion.easing.Back;
+import com.eclecticdesignstudio.motion.easing.Cubic;
+import com.eclecticdesignstudio.motion.easing.Elastic;
+import com.eclecticdesignstudio.motion.easing.Expo;
+import com.eclecticdesignstudio.motion.easing.Linear;
+import com.eclecticdesignstudio.motion.easing.Quad;
+import com.eclecticdesignstudio.motion.easing.Quart;
+import com.eclecticdesignstudio.motion.easing.Quint;
+import com.eclecticdesignstudio.motion.easing.Sine;
+
+
+
+class Design_74_74_WalkingPM extends ActorScript
+{          	
+	
+public var _Acceleration:Float;
+
+public var _MaxWalkingSpeed:Float;
+
+public var _LimitSpeed:Bool;
+
+public var _WalkRightAnimation:String;
+
+public var _WalkLeftAnimation:String;
+
+public var _IdleRightAnimation:String;
+
+public var _IdleLeftAnimation:String;
+
+public var _PreventWalking:Bool;
+
+public var _DisableStatuses:Array<Dynamic>;
+
+public var _AnimationCategory:String;
+
+public var _ShouldPlaySound:Bool;
+
+public var _FootstepSounds:Array<Dynamic>;
+    
+
+/* Params are:__Self */
+public function _customBlock_AtMaxWalkingSpeed():Bool
+{
+var __Self:Actor = actor;
+        if((asBoolean(actor.getActorValue("Facing Right?")) && (actor.getXVelocity() >= _MaxWalkingSpeed)))
+{
+            if(_LimitSpeed)
+{
+                actor.setXVelocity(_MaxWalkingSpeed);
+}
+
+            return true;
+}
+
+        if((!(asBoolean(actor.getActorValue("Facing Right?"))) && (actor.getXVelocity() <= -(_MaxWalkingSpeed))))
+{
+            if(_LimitSpeed)
+{
+                actor.setXVelocity(-(_MaxWalkingSpeed));
+}
+
+            return true;
+}
+
+        return false;
+}
+    
+
+/* Params are:__Self __Accel */
+public function _customBlock_WalkAccel(__Accel:Float):Void
+{
+var __Self:Actor = actor;
+        _Acceleration = asNumber(__Accel);
+propertyChanged("_Acceleration", _Acceleration);
+}
+    
+
+/* Params are:__Self */
+public function _customBlock_GetWalkAccel():Float
+{
+var __Self:Actor = actor;
+        return _Acceleration;
+}
+    
+
+/* Params are:__Self __MaxSpeed */
+public function _customBlock_MaxWalkSpeed(__MaxSpeed:Float):Void
+{
+var __Self:Actor = actor;
+        _MaxWalkingSpeed = asNumber(__MaxSpeed);
+propertyChanged("_MaxWalkingSpeed", _MaxWalkingSpeed);
+}
+    
+
+/* Params are:__Self */
+public function _customBlock_GetMaxWalkSpeed():Float
+{
+var __Self:Actor = actor;
+        return _MaxWalkingSpeed;
+}
+    
+
+/* Params are:__Self __Prevent */
+public function _customBlock_PreventWalk(__Prevent:Bool):Void
+{
+var __Self:Actor = actor;
+        _PreventWalking = __Prevent;
+propertyChanged("_PreventWalking", _PreventWalking);
+}
+    
+
+/* Params are:__Self __Limit */
+public function _customBlock_LimitSpeed(__Limit:Bool):Void
+{
+var __Self:Actor = actor;
+        _LimitSpeed = __Limit;
+propertyChanged("_LimitSpeed", _LimitSpeed);
+}
+    
+
+/* Params are:__Self __Rate */
+public function _customBlock_SlowToMax(__Rate:Float):Void
+{
+var __Self:Actor = actor;
+        if((Math.abs(__Self.getXVelocity()) > _MaxWalkingSpeed))
+{
+            actor.setXVelocity((actor.getXVelocity() * __Rate));
+}
+
+}
+    
+
+/* Params are: */
+public function _customBlock_PlayFootsteps():Void
+{
+var __Self:Actor = actor;
+        if(_ShouldPlaySound)
+{
+            _ShouldPlaySound = false;
+propertyChanged("_ShouldPlaySound", _ShouldPlaySound);
+            runLater(1000 * 0.3, function(timeTask:TimedTask):Void {
+                        _ShouldPlaySound = true;
+propertyChanged("_ShouldPlaySound", _ShouldPlaySound);
+}, actor);
+            if(!((_FootstepSounds.length == 0)))
+{
+                playSound(cast((scripts.Design_207_207_StencylExtrasPM._customBlock_GetNamedSound((scripts.Design_207_207_StencylExtrasPM._customBlock_GetRandomItemFromList(_FootstepSounds)))), Sound));
+}
+
+}
+
+}
+
+ 
+ 	public function new(dummy:Int, actor:Actor, engine:Engine)
+	{
+		super(actor, engine);	
+		nameMap.set("Acceleration", "_Acceleration");
+_Acceleration = 85.0;
+nameMap.set("Max Walking Speed", "_MaxWalkingSpeed");
+_MaxWalkingSpeed = 20.0;
+nameMap.set("Limit Speed?", "_LimitSpeed");
+_LimitSpeed = false;
+nameMap.set("Walk Right Animation", "_WalkRightAnimation");
+nameMap.set("Walk Left Animation", "_WalkLeftAnimation");
+nameMap.set("Idle Right Animation", "_IdleRightAnimation");
+nameMap.set("Idle Left Animation", "_IdleLeftAnimation");
+nameMap.set("Prevent Walking?", "_PreventWalking");
+_PreventWalking = false;
+nameMap.set("Disable Statuses", "_DisableStatuses");
+_DisableStatuses = [];
+nameMap.set("Animation Category", "_AnimationCategory");
+_AnimationCategory = "Walking";
+nameMap.set("Should Play Sound?", "_ShouldPlaySound");
+_ShouldPlaySound = false;
+nameMap.set("Footstep Sounds", "_FootstepSounds");
+_FootstepSounds = [];
+nameMap.set("Actor", "actor");
+
+	}
+	
+	override public function init()
+	{
+		            /* "Inputs: ----------------------" */
+        /* "\"On Ground?\" -- <Boolean> Actor Level Attribute, from \"On Ground\" Behavior (required)" */
+        /* "\"Is Running?\" -- <Boolean> Actor Level Attribute, from \"PM Jumping\" Behavior (NOT required)" */
+        /* "\"Is Ducking?\" -- <Boolean> Actor Level Attribute, from \"PM Ducking\" Behavior (NOT required)" */
+        /* "\"Is Slope Sliding?\" -- <Boolean> Actor Level Attribute, from \"Slope Detection\" Behavior (NOT required)" */
+        /* "Outputs: ---------------------" */
+        /* "\"Facing Right?\" -- <Boolean> Actor Level Attribute" */
+        actor.setActorValue("Facing Right?", true);
+        _ShouldPlaySound = true;
+propertyChanged("_ShouldPlaySound", _ShouldPlaySound);
+    addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void {
+if(wrapper.enabled){
+        for(item in cast(_DisableStatuses, Array<Dynamic>))
+{
+            if(asBoolean(actor.getActorValue(("" + item))))
+{
+                return;
+}
+
+}
+
+        if(_PreventWalking)
+{
+            return;
+}
+
+        /* "Custom: Is \"Right\" on for Self, Is \"Left\" on for Self" */
+        if((cast((actor.say("Control Adapter PM", "_customBlock_ControlIsOn", ["Right"])), Bool) && !(cast((actor.say("Control Adapter PM", "_customBlock_ControlIsOn", ["Left"])), Bool))))
+{
+            actor.setActorValue("Facing Right?", true);
+            if(asBoolean(actor.getActorValue("On Ground?")))
+{
+                /* "Loop <Walk Right Animation> for Self Using <Animation Category>" */
+                scripts.Design_27_27_ActorExtrasPM._customBlock_SafeLoopAnimation(actor,_WalkRightAnimation,_AnimationCategory);
+                actor.say("Walking PM", "_customBlock_PlayFootsteps");
+}
+
+            /* "Custom: Self At Max Walking Speed" */
+            if(cast((actor.say("Walking PM", "_customBlock_AtMaxWalkingSpeed")), Bool))
+{
+                return;
+}
+
+            actor.push(1, 0, _Acceleration);
+}
+
+        /* "Custom: Is \"Left\" on for Self, Is \"Right\" on for Self" */
+        if((cast((actor.say("Control Adapter PM", "_customBlock_ControlIsOn", ["Left"])), Bool) && !(cast((actor.say("Control Adapter PM", "_customBlock_ControlIsOn", ["Right"])), Bool))))
+{
+            actor.setActorValue("Facing Right?", false);
+            if(asBoolean(actor.getActorValue("On Ground?")))
+{
+                /* "Custom: Loop <Walk Left Animation> for Self Using <Animation Category>" */
+                scripts.Design_27_27_ActorExtrasPM._customBlock_SafeLoopAnimation(actor,_WalkLeftAnimation,_AnimationCategory);
+                actor.say("Walking PM", "_customBlock_PlayFootsteps");
+}
+
+            /* "Custom: Self At Max Walking Speed" */
+            if(cast((actor.say("Walking PM", "_customBlock_AtMaxWalkingSpeed")), Bool))
+{
+                return;
+}
+
+            actor.push(-1, 0, _Acceleration);
+}
+
+        /* "Custom: Is \"Right\" on for Self, Is \"Left\" on for Self" */
+        if((!(cast((actor.say("Control Adapter PM", "_customBlock_ControlIsOn", ["Right"])), Bool)) && !(cast((actor.say("Control Adapter PM", "_customBlock_ControlIsOn", ["Left"])), Bool))))
+{
+            _ShouldPlaySound = true;
+propertyChanged("_ShouldPlaySound", _ShouldPlaySound);
+            if(asBoolean(actor.getActorValue("On Ground?")))
+{
+                if(asBoolean(actor.getActorValue("Facing Right?")))
+{
+                    /* "Loop <Idle Right Animation> for Self Using <Animation Category>" */
+                    scripts.Design_27_27_ActorExtrasPM._customBlock_SafeLoopAnimation(actor,_IdleRightAnimation,_AnimationCategory);
+}
+
+                else
+{
+                    /* "Loop <Idle Left Animation> for Self Using <Animation Category>" */
+                    scripts.Design_27_27_ActorExtrasPM._customBlock_SafeLoopAnimation(actor,_IdleLeftAnimation,_AnimationCategory);
+}
+
+}
+
+}
+
+}
+});
+
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
---
+++ behavior-hx/SceneEvents_32.hx
@@ -0,0 +1,150 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.events.Event;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import com.eclecticdesignstudio.motion.Actuate;
+import com.eclecticdesignstudio.motion.easing.Back;
+import com.eclecticdesignstudio.motion.easing.Cubic;
+import com.eclecticdesignstudio.motion.easing.Elastic;
+import com.eclecticdesignstudio.motion.easing.Expo;
+import com.eclecticdesignstudio.motion.easing.Linear;
+import com.eclecticdesignstudio.motion.easing.Quad;
+import com.eclecticdesignstudio.motion.easing.Quart;
+import com.eclecticdesignstudio.motion.easing.Quint;
+import com.eclecticdesignstudio.motion.easing.Sine;
+
+
+
+class SceneEvents_32 extends SceneScript
+{          	
+	
+public var _ExitName:String;
+
+ 
+ 	public function new(dummy:Int, engine:Engine)
+	{
+		super(engine);
+		nameMap.set("Exit Name", "_ExitName");
+_ExitName = "";
+
+	}
+	
+	override public function init()
+	{
+		    addActorEntersRegionListener(getRegion(2), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorType(156),a.getType(),a.getGroup())){
+        getActor(14).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.75, function(timeTask:TimedTask):Void {
+                    getActor(14).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(3), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorType(156),a.getType(),a.getGroup())){
+        getActor(19).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.25, function(timeTask:TimedTask):Void {
+                    getActor(19).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(0), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorType(156),a.getType(),a.getGroup())){
+        getActor(21).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.75, function(timeTask:TimedTask):Void {
+                    getActor(21).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(1), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorType(156),a.getType(),a.getGroup())){
+        getActor(20).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.25, function(timeTask:TimedTask):Void {
+                    getActor(20).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(2), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorType(9),a.getType(),a.getGroup())){
+        getActor(14).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.75, function(timeTask:TimedTask):Void {
+                    getActor(14).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(0), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorType(9),a.getType(),a.getGroup())){
+        getActor(21).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.75, function(timeTask:TimedTask):Void {
+                    getActor(21).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(2), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorGroup(8),a.getType(),a.getGroup())){
+        getActor(14).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.75, function(timeTask:TimedTask):Void {
+                    getActor(14).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(0), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorGroup(8),a.getType(),a.getGroup())){
+        getActor(21).say("Activate On Event PM", "_customEvent_" + "Activate");
+        runLater(1000 * 0.75, function(timeTask:TimedTask):Void {
+                    getActor(21).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(1), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorGroup(8),a.getType(),a.getGroup())){
+        getActor(20).say("Activate On Event PM", "_customEvent_" + "Activate");
+        a.setActorValue("Facing Right?", true);
+        runLater(1000 * 0.25, function(timeTask:TimedTask):Void {
+                    getActor(20).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+    addActorEntersRegionListener(getRegion(3), function(a:Actor, list:Array<Dynamic>):Void  {
+if(wrapper.enabled && sameAsAny(getActorGroup(8),a.getType(),a.getGroup())){
+        getActor(19).say("Activate On Event PM", "_customEvent_" + "Activate");
+        a.setActorValue("Facing Right?", false);
+        runLater(1000 * 0.25, function(timeTask:TimedTask):Void {
+                    getActor(19).say("Activate On Event PM", "_customEvent_" + "Deactivate");
+}, null);
+}
+});
+
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
