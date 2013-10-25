c5c643440ffff25be1280f08d86e5e6314679cdb
Tim Frost <tim@timf.me>
2013-10-25 16:57:47 +0100

    All Farm BGs updated with new text PM Text 2 tile set added

---
+++ behavior-hx/ActorEvents_232.hx
@@ -0,0 +1,65 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.events.Event;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import com.eclecticdesignstudio.motion.Actuate;
+import com.eclecticdesignstudio.motion.easing.Back;
+import com.eclecticdesignstudio.motion.easing.Cubic;
+import com.eclecticdesignstudio.motion.easing.Elastic;
+import com.eclecticdesignstudio.motion.easing.Expo;
+import com.eclecticdesignstudio.motion.easing.Linear;
+import com.eclecticdesignstudio.motion.easing.Quad;
+import com.eclecticdesignstudio.motion.easing.Quart;
+import com.eclecticdesignstudio.motion.easing.Quint;
+import com.eclecticdesignstudio.motion.easing.Sine;
+
+
+
+class ActorEvents_232 extends ActorScript
+{          	
+	
+ 
+ 	public function new(dummy:Int, actor:Actor, engine:Engine)
+	{
+		super(actor, engine);	
+		
+	}
+	
+	override public function init()
+	{
+		            actor.disableActorDrawing();
+
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
