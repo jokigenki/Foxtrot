1c342d10e99d51184024beb6db6cd58d9997240a
Tim Frost <tim@timf.me>
2013-11-13 17:22:06 +0000

    Farm 1_2 updated

---
+++ behavior-hx/Design_333_333_MusicTogglePM.hx
@@ -0,0 +1,92 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.events.Event;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import com.eclecticdesignstudio.motion.Actuate;
+import com.eclecticdesignstudio.motion.easing.Back;
+import com.eclecticdesignstudio.motion.easing.Cubic;
+import com.eclecticdesignstudio.motion.easing.Elastic;
+import com.eclecticdesignstudio.motion.easing.Expo;
+import com.eclecticdesignstudio.motion.easing.Linear;
+import com.eclecticdesignstudio.motion.easing.Quad;
+import com.eclecticdesignstudio.motion.easing.Quart;
+import com.eclecticdesignstudio.motion.easing.Quint;
+import com.eclecticdesignstudio.motion.easing.Sine;
+
+
+
+class Design_333_333_MusicTogglePM extends ActorScript
+{          	
+	    public function _customEvent_MusicToggle():Void
+{
+        if(getGameAttribute("Is Music On?"))
+{
+            sayToScene("Sound Manager PM", "_customBlock_StopCurrentMusic");
+}
+
+        else
+{
+            sayToScene("Sound Manager PM", "_customBlock_LoopCurrentMusic");
+}
+
+        saveGame("mySave", function(success:Bool):Void {
+
+});
+}
+
+    public function _customEvent_MusicOff():Void
+{
+        actor.setAnimation("" + "Inactive");
+}
+
+    public function _customEvent_MusicOn():Void
+{
+        actor.setAnimation("" + "Active");
+}
+
+
+ 
+ 	public function new(dummy:Int, actor:Actor, engine:Engine)
+	{
+		super(actor, engine);	
+		nameMap.set("Actor", "actor");
+
+	}
+	
+	override public function init()
+	{
+		
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
